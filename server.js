1306c3753f9d85e4f2ac785e1e3a382037df390e
Brian Stanback <stanback@gmail.com>
2014-01-28 01:18:17 -0800

    Implemented new ready checking in JSDom and Phantom

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var server = prerender({
   renderPoolMaxQueueSize: process.env.PRERENDER_RENDER_POOL_MAX_QUEUE_SIZE || 10000, // Maximum number of requests to queue
   renderPoolCheckInterval: process.env.PRERENDER_RENDER_CHECK_INTERVAL || 100, // Milliseconds to wait before checking the queue for new jobs
 
-  engine: process.env.PRERENDER_ENGINE || 'phantom', // Can be: jsdom, phantom
+  engine: process.env.PRERENDER_ENGINE || 'jsdom', // Can be: jsdom, phantom
   renderTimeout: process.env.PRERENDER_RENDER_TIMEOUT || 20000, // Milliseconds to wait before a render job is considered a failure
   maxRequestsPerRenderer: process.env.PRERENDER_REQUESTS_PER_RENDERER || 100, // Maximum number of requests a worker can handle before restarting it
 
@@ -17,7 +17,7 @@ var server = prerender({
   assetsPath: process.env.PRERENDER_ASSETS_PATH || '/Users/brian/Documents/Projects/stylespotter-web/public/', // Path to your app's files
   applicationPage: process.env.PRERENDER_APPLICATION_PAGE || 'index.html', // Main application page
   serveFiles: process.env.PRERENDER_SERVE_FILES || true, // Serve static files
-  filesMatch: process.env.PRERENDER_FILES_MATCH || /\.(css|js|jpg|png|gif|ico|svg|woff|ttf)$/, // List of static file patterns
+  filesMatch: process.env.PRERENDER_FILES_MATCH || /\.(?:css|js|jpg|png|gif|ico|svg|woff|ttf)(?:\?|$)/, // List of static file patterns
 
   logging: {
     level: process.env.PRERENDER_LOG_LEVEL || 'debug', // Logging verbosity
