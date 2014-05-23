5a0cc8c9a232259205f57dcee160ad2bd18ec3e4
Brian Stanback <email@brianstanback.com>
2014-05-22 18:50:07 -0700

    Passing two arguments into append instead of two separate calls

--- example/app/initializers/meta.js
+++ example/app/initializers/meta.js
@@ -2,15 +2,16 @@ export default {
   name: 'meta',
   initialize: function() {
     $(function() {
-      $('head').append($('<script>', {
-        id: 'meta-start',
-        type: 'text/x-placeholder'
-      }));
-
-      $('head').append($('<script>', {
-        id: 'meta-end',
-        type: 'text/x-placeholder'
-      }));
+      $('head').append(
+        $('<script>', {
+          id: 'meta-start',
+          type: 'text/x-placeholder'
+        }),
+        $('<script>', {
+          id: 'meta-end',
+          type: 'text/x-placeholder'
+        })
+      );
     });
   }
 };
