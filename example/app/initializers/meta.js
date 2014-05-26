a01e041855cb8ccbdf47b7738f441d4096cbe273
Brian Stanback <stanback@gmail.com>
2014-05-26 00:54:01 -0700

    Initializer wasnt playing nicley with jsdom

--- example/app/initializers/meta.js
+++ example/app/initializers/meta.js
@@ -2,18 +2,16 @@ export default {
   name: 'meta',
   initialize: function(controller, application) {
     application.META_TITLE_DEFAULT = document.title;
-    $(function() {
-      $('head').append(
-        $('<script>', {
-          id: 'meta-start',
-          type: 'text/x-placeholder'
-        }),
-        $('<script>', {
-          id: 'meta-end',
-          type: 'text/x-placeholder'
-        })
-      );
-    });
+    $('head').append(
+      $('<script>', {
+        id: 'meta-start',
+        type: 'text/x-placeholder'
+      }),
+      $('<script>', {
+        id: 'meta-end',
+        type: 'text/x-placeholder'
+      })
+    );
   }
 };
 
