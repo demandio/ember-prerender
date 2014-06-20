372cb5f0921d542c7cb90ad2bfd43556e4a6d414
Brian Stanback <email@brianstanback.com>
2014-06-20 15:41:55 -0700

    Fixing jsdom error handling

--- example/app/controllers/crash.js
+++ example/app/controllers/crash.js
@@ -2,7 +2,7 @@ export default Ember.Controller.extend({
   init: function() {
     console.log("Throwing error in 1 second...");
     Em.run.later(function() {
-      throw "An error occurred";
+      throw new Error("An error occurred");
     }, 1000);
   }
 });
