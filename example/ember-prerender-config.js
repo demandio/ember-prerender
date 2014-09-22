8d7b47a64cb71df98ba78214abb4b872f16ae17a
Brian Stanback <email@brianstanback.com>
2014-09-22 16:50:43 -0700

    Removing unnecessary replace logic

--- example/ember-prerender-config.js
+++ example/ember-prerender-config.js
@@ -2,7 +2,7 @@ module.exports = {
   engine: 'phantom',
   //engine: 'jsdom',
   //engine: 'webdriver',
-  baseUrl: 'http://localhost:4200/',
+  appUrl: 'http://localhost:4200/',
   plugins: [
       'removeScriptTags',
       'httpHeaders'
